ce435a45f07f264cd4f4598f99a9fb81f4e12152
StackerGames <144170198+StackerGames@users.noreply.github.com>
2023-09-05 16:55:42 -0300

    is changed

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
 	name: "The Incrementaling Tree",
 	id: "tit",
 	author: "Stacker Games",
-	pointsName: "points",
+	pointsName: "sands",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -57,7 +57,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("e1000000"))
 }
 
 
